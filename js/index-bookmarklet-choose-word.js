2324fcb335c890e1030aef614f5666220606847b
John Chase <john@theartoftechllc.com>
2022-04-04 15:35:19 -0600

    working with p tags

--- js/index-bookmarklet-choose-word.js
+++ js/index-bookmarklet-choose-word.js
@@ -2,7 +2,7 @@
     /*inject styles*/
     const styleElem = document.head.appendChild(document.createElement("style"));
     styleElem.innerHTML = `
-    /*GENERAL*/ .p { border: 1px solid dodgerblue; border-radius: .5rem; padding: .5rem; margin: 1rem 5rem; font-size: 15px; line-height: 21px; } .hidden { display: none; } /*DEFINITION GROUP*/ .def-div { background-color: dodgerblue; position: fixed; z-index: 99999999; min-width: 100%; height: 32px; top:0;text-align: left;} .def-span { padding: 0 0 12px 4px; text-align: center; color: blue; font-weight: bold; font-style: italic; margin: 0.5rem; } .def-help { font-style: italic; margin: 0.5rem; } /*TOOLTIPS*/ .def-tooltip { position: relative; transition: all 0.3s ease; text-decoration: none; cursor: grabbing; } .def-tooltip:before { content: ""; position: absolute; opacity: 0; pointer-events: none; left: 50%; transform: translate3d(-50%, 0%, 0); transition: all 0.3s ease; transition-delay: 1.2s; width: 0; height: 0; border-style: solid; border-width: 10px 10px 0 10px; border-color: dodgerblue transparent transparent transparent; } .def-tooltip:after { text-transform: none; content: attr(data-tooltip); font-size: 12px; line-height: 18px; position: absolute; color: white; font-weight: bold; background: dodgerblue; padding: 8px 12px; width: -webkit-max-content; width: -moz-max-content; width: max-content; max-width: 200px; opacity: 0; pointer-events: none; left: 50%; top: 0; border-radius: 4px; transform: translate3d(-50%, 0%, 0); transition: all 0.3s ease; transition-delay: 1.2s; } .def-tooltip:hover { background-color: rgba(0, 0, 0, 0.12); } .def-tooltip:hover:before, .def-tooltip:hover:after { opacity: 1; } .def-tooltip:hover:before { transform: translate3d(-50%, calc(-50% - 4px), 0); } .def-tooltip:hover:after { transform: translate3d(-50%, calc(-80% - 16px), 0); } .def-tooltip .def-tooltip-content::after { background-color: #05a8ff; content: ""; height: 10px; position: absolute; transform: rotate(45deg); width: 10px; } .def-tooltip-not-found:after { color: white; font-weight: bold; background: red; } .def-tooltip-not-found:before { border-color: red transparent transparent transparent; } /*TOGGLE SWITCH*/ input[type=checkbox], .def-input { margin: 0; -webkit-appearance: none; appearance: none; padding: 0 0 0 30px; border-radius: 16px; background: radial-gradient(circle 12px, white 100%, transparent calc(100% + 1px)) #ccc -16px; transition: 0.3s ease-in-out; position: inherit; height: 32px; width: 64px; top: 0; bottom: 0; box-shadow: none; } .def-input::before { content: "OFF"; font: bold 12px/32px Verdana; color: white; text-shadow: 0 1px black; } .def-input:checked { padding: 0 0 0 8px; background-color: rgb(37, 151, 62); background-position: 16px; height: 32px; width: 64px; } .def-input:checked::before { content: "ON"; }
+/*GENERAL*/ .p { border: 1px solid dodgerblue; border-radius: .5rem; padding: .5rem; margin: 1rem 5rem; font-size: 15px; line-height: 1.2;} .hidden { display: none; } /*DEFINITION GROUP*/ .def-div { background-color: dodgerblue; position:fixed; z-index:99999999; min-width:100% } .def-span { padding: 0 0 12px 4px; text-align: center; color: blue; font-weight: bold; font-style: italic; margin: 0.5rem; } .def-help { font-style: italic; margin: 0.5rem; } /*TOOLTIPS*/ .def-tooltip { position: relative; transition: all 0.3s ease; text-decoration: none; cursor: grabbing; } .def-tooltip:before { content: ""; position: absolute; opacity: 0; pointer-events: none; left: 50%; transform: translate3d(-50%, 0%, 0); transition: all 0.3s ease; transition-delay: 1.2s; width: 0; height: 0; border-style: solid; border-width: 10px 10px 0 10px; border-color: dodgerblue transparent transparent transparent; } .def-tooltip:after { text-transform: none; content: attr(data-tooltip); font-size: 11px; position: absolute; color:white; font-weight:bold;background: dodgerblue; padding: 8px 12px; width: -webkit-max-content; width: -moz-max-content; width: max-content; max-width: 200px; opacity: 0; pointer-events: none; left: 50%; top: 0; border-radius: 4px; transform: translate3d(-50%, 0%, 0); transition: all 0.3s ease; transition-delay: 1.2s; } .def-tooltip:hover { background-color: rgba(0, 0, 0, 0.12); } .def-tooltip:hover:before, .def-tooltip:hover:after { opacity: 1; } .def-tooltip:hover:before { transform: translate3d(-50%, calc(-50% - 4px), 0); } .def-tooltip:hover:after { transform: translate3d(-50%, calc(-80% - 16px), 0); } .def-tooltip .def-tooltip-content::after { background-color: #05a8ff; content: ""; height: 10px; position: absolute; transform: rotate(45deg); width: 10px; } .def-tooltip-not-found:after { background: red; color:white; font-weight:bold;} .def-tooltip-not-found:before { border-color: red transparent transparent transparent; } /*TOGGLE SWITCH*/ input[type=checkbox], .def-input { margin: 0; -webkit-appearance: none; appearance: none; padding-left: 30px; box-shadow: none; border-radius: 16px; background: radial-gradient(circle 12px, white 100%, transparent calc(100% + 1px)) #ccc -16px; transition: 0.3s ease-in-out; position: inherit; height: 32px; width: 64px; top: 0; bottom: 0;} .def-input::before { content: "OFF"; font: bold 12px/32px Verdana; color: white; text-shadow: 0 1px black; } .def-input:checked { padding-left: 8px; background-color: rgb(37, 151, 62); background-position: 16px; height: 32px; width: 64px; } .def-input:checked::before { content: "ON"; } 
 `;
     /*set up elements*/
     const body = document.getElementsByTagName("body")[0];
@@ -21,18 +21,46 @@
     defSwitch.appendChild(defToggle);
     const defHelp = document.createElement("span");
     defHelp.classList.add('hidden', 'def-help');
-    defHelp.innerText = "Hover over a word to highlight, then click/tap to show definition tooltip.";
+    defHelp.innerText = "Hover over a word to highlight, then click/tap to show definition tooltip. Where a word has punctuation, a [list] has been created to allow other choices.";
     defSwitch.appendChild(defHelp);
     let spans;
     let p = document.getElementsByTagName("p");
     let originalP = [];
+    /*if punctuation, break into strings containing unique punct forms*/
+    // const checkPunct = (word) => {
+    //     console.log(word + "\n");
+    //     let newWord = word;
+    //     const punctuationMarks = ["'", ".", "-", ",", '"'];
+    //     [...punctuationMarks].forEach(mark => {
+    //         console.log(mark);
+    //         let punct = word.indexOf(mark)
+    //         console.log(punct);
+    //         if (punct !== -1) {
+    //             if (mark === '-') {
+    //                 newWord += " " + word.split(mark).join(' ');
+    //             } else
+    //                 newWord += " " + word.split(mark).join('');
+    //         }
+    //     });
+    //     if (newWord === word) {
+    //         return word;
+    //     } else {
+    //         return newWord;
+    //     }
+    // }
     /*break p into words, wrap with spans*/
     const addSpans = (paragraph) => {
-        paragraph = paragraph.replace('&nbsp;', ' '); //!!!no workey
         let words = paragraph.split(" ");
         let newParagraph = '';
         words.forEach(word => {
+            // if (word.includes("-")) {
+            //     hyphen = word.split("-");
+            //     hyphen.forEach(noHyphen => {
+            //         newParagraph += `<span>${noHyphen}</span> `;
+            //     })
+            // } else {
             newParagraph += `<span>${word}</span> `;
+            // }
         });
         return newParagraph;
     }
@@ -44,6 +72,8 @@
             for (let i = 0; i < p.length; i++) {
                 if (p[i] == undefined || p[i].innerText === '') continue;
                 p[i].classList.add("p");
+                // console.log("innerHTML: ", p[i].innerHTML);
+                // console.log("innerText: ", p[i].innerText);
                 originalP.push(p[i].innerHTML);
                 p[i].innerHTML = addSpans(p[i].innerText);
                 spans = p[i].getElementsByTagName("span");
@@ -64,6 +94,7 @@
         }
     }
 })();
+
 /*fetch word*/
 const wordLookupRequest = async (lookup) => {
     lookup = lookup.replace(/[\u2000-\u206F\u2E00-\u2E7F!"#$%&()*+,\.\/:;<=>?@\[\]^_`{|}~]/g, ''); //all punct but - and '
@@ -93,15 +124,18 @@ const wordLookupRequest = async (lookup) => {
     }
     // }
 }
+
 /*highlight the word*/
 function onmouseoverspan() {
     this.style.backgroundColor = "yellow";
 }
+
 /*unhighlight word, remove tooltip*/
 function onmouseoutspan() {
     this.style.backgroundColor = "transparent";
     this.classList.remove('def-tooltip');
 }
+
 /*fetch word, then add data/class*/
 function onclickspan(e) {
     const tag = e.target;
